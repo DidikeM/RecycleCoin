c23a49b36388029afa1ce801fc3358d6b3820bda
lordwile <lordwilee@outlook.com>
2022-12-18 19:13:23 +0300

    coin service client added

---
+++ RecycleCoin.API/Concrete/CoinTransferServiceClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using RecycleCoin.API.Abstract;
+
+namespace RecycleCoin.API.Concrete
+{
+    public class CoinTransferServiceClient:ClientBase<ICoinTransferServiceClient>
+    {
+
+        public CoinTransferServiceClient(Binding binding, EndpointAddress remoteAddress):base(binding, remoteAddress) { }
+        public string CoinTransfer(string address, int coinAmount) => Channel.CoinTransfer(address, coinAmount);
+        
+
+    }
+}
--- test/Test.cs
+++ test/Test.cs
@@ -1,9 +1,11 @@
-﻿using RecycleCoin.Business.Abstract;
+﻿using RecycleCoin.API.Concrete;
+using RecycleCoin.Business.Abstract;
 using RecycleCoin.Business.DependencyResolvers.Ninject;
 using RecycleCoin.Entities.Concrete.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +21,9 @@ namespace test
 
         public Test() 
         {
-            
+            var a = new CoinTransferServiceClient(new BasicHttpBinding(), new EndpointAddress("http://127.0.0.1:7335"));
+            string sonuc = a.CoinTransfer("RVXj2FwF4aqFSGf29QqTEpXacgjM7V891H", 3);
+            Console.WriteLine(sonuc);
         }
     }
 }
