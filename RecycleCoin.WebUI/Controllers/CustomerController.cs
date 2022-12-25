78f94e0b8d4291e0a31ef9b30395b2c815707641
DidikeM <91502037+DidikeM@users.noreply.github.com>
2022-12-25 17:33:43 +0300

    _Layout fixed

--- RecycleCoin.WebUI/Controllers/CustomerController.cs
+++ RecycleCoin.WebUI/Controllers/CustomerController.cs
@@ -4,6 +4,11 @@ namespace RecycleCoin.WebUI.Controllers
 {
     public class CustomerController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         public IActionResult WithDraw()
         {
             return View();
